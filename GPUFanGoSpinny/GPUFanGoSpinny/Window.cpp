ad6ff8537cd6da12d84f780a30af3fa6616d57af
Jesper Sips <jesper.sips@outlook.com>
2024-03-01 17:02:20 +0100

    [ADDED] Resize support
    you can now resize yay

--- GPUFanGoSpinny/GPUFanGoSpinny/Window.cpp
+++ GPUFanGoSpinny/GPUFanGoSpinny/Window.cpp
@@ -1,12 +1,18 @@
 #include "pch.h"
 #include "Window.h"
 
+void OnWindowResize(GLFWwindow* p_window, int p_width, int p_height)
+{
+	glViewport(0, 0, p_width, p_height);
+}
+
 Window::Window(UINT32 p_width, UINT32 p_height, std::string p_title)
 {
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
 	
 	m_window = glfwCreateWindow(p_width, p_height, p_title.c_str(), nullptr, nullptr);
+	glfwSetWindowSizeCallback(m_window, OnWindowResize);
 
 	if (!m_window)
 	{
