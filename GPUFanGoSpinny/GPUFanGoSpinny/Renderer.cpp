ff61646fbe59e912df325f1c613ffd09f7fb0e0a
Jesper Sips <jesper.sips@outlook.com>
2024-03-19 14:26:11 +0100

    [FIXED] Fixed quad not showing up
    Fixed a few issues:
    -EXE file couldnt find the shader files, they now get build in the post-build event
    -vertex data wasnt correctly being sent to the shader, i fixed that but i dont remember how?

--- GPUFanGoSpinny/GPUFanGoSpinny/Renderer.cpp
+++ GPUFanGoSpinny/GPUFanGoSpinny/Renderer.cpp
@@ -31,11 +31,19 @@ void Renderer::Initialize(HINSTANCE p_hInstance, int p_width, int p_height)
 
 	glViewport(0, 0, p_width, p_height);
 
-	unsigned int VAO = 0;
+	GLuint VBO = 0, EBO = 0, VAO = 0;
+	glGenBuffers(1, &VBO);
+	glGenBuffers(1, &EBO);
 
 	glGenVertexArrays(1, &VAO);
 
 	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex), nullptr, GL_STATIC_DRAW);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint), nullptr, GL_STATIC_DRAW);
 
 	// position attribute
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)0);
@@ -49,11 +57,11 @@ void Renderer::Initialize(HINSTANCE p_hInstance, int p_width, int p_height)
 	glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, 8 * sizeof(float), (void*)(6 * sizeof(float)));
 	glEnableVertexAttribArray(2);
 
+	m_VBO = VBO;
+	m_EBO = EBO;
 	m_VAO = VAO;
 
 	m_shader = new Shader("Shaders/vshader.vs", "Shaders/fshader.fs");
-
-
 }
 
 bool Renderer::IsQuitting()
@@ -68,13 +76,11 @@ void Renderer::Render()
 
 	m_shader->use();
 
-	glBindVertexArray(m_VAO);
-
 	// Check for OpenGL errors
 	GLenum error;
 	while ((error = glGetError()) != GL_NO_ERROR) {
 		std::cerr << "OpenGL error before draw call: " << error << std::endl;
-		throw;
+		//throw;
 	}
 
 	glDrawElements(GL_TRIANGLES, m_mesh->getIndices().size(), GL_UNSIGNED_INT, 0);
@@ -82,12 +88,10 @@ void Renderer::Render()
 	// Check for OpenGL errors
 	while ((error = glGetError()) != GL_NO_ERROR) {
 		std::cerr << "OpenGL error after draw call: " << error << std::endl;
-		throw;
+		//throw;
 	}
 
 	glGetError();
-
-	glBindVertexArray(0);
 }
 
 void Renderer::Update()
@@ -105,17 +109,12 @@ void Renderer::AddMesh(Mesh* p_mesh)
 {
 	m_mesh = p_mesh;
 
-	GLuint VBO = 0, EBO = 0;
+	//glGenBuffers(1, &m_VBO);
+	//glGenBuffers(1, &m_EBO);
 
-	glGenBuffers(1, &VBO);
-	glGenBuffers(1, &EBO);
-
-	m_VBO = VBO;
-	m_EBO = EBO;
-
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBindBuffer(GL_ARRAY_BUFFER, m_VBO);
 	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * m_mesh->getVertexData().size(), m_mesh->getVertexData().data(), GL_STATIC_DRAW);
 
-	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(GLuint) * m_mesh->getIndices().size(), m_mesh->getIndices().data(), GL_STATIC_DRAW);
 }
